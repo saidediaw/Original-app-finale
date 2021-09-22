5abc612145bd43e9baae1c5438eed23b7b8363f7
saidediaw <saidediaw2010@yahoo.com>
2021-09-22 19:44:26 +0900

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_093724) do
+ActiveRecord::Schema.define(version: 2021_09_22_104058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_09_17_093724) do
     t.string "name"
     t.string "city"
     t.integer "phone"
+    t.boolean "guest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
