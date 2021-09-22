b746eef6750531034dc76c1dbf816d96d3a1f976
saidediaw <saidediaw2010@yahoo.com>
2021-09-22 19:21:55 +0900

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,14 @@
 # User.create(name: "Saqr", city: "Saint Louis", phone: '774561234', role: 'Owner', email: "saqr@gmail.com", password: "saqr210", password_confirmation: "saqr210")
 # User.create(name: "Talha", city: "Dakar", phone: '709871234', role: 'Owner', email: "talha@gmail.com", password: "talha210", password_confirmation: "talha210")
 # User.create(name: "Zubayr", city: "Saint Louis", phone: '770345678', role: 'Owner', email: "zubayr@gmail.com", password: "zubayr210", password_confirmation: "zubayr210")
+User.create!(name: 'Guest User',
+             email: 'guest@example.com',
+             password: '12345678',
+             password_confirmation: '12345678',
+             created_at: Time.zone.now,
+             updated_at: Time.zone.now,
+             guest: true)
+
 user = User.find_or_initialize_by(email: 'admin@gmail.com')
 user.name = 'Admin'
 user.password = 'admin@12345'
